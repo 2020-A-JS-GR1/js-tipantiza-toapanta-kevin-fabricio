e9386dd1769ce7ddd782d495f5f4105ea994d961
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-06-12 17:19:50 -0500

    Variables con console typeof

---
+++ 01-Javascript/01-variables.js
@@ -0,0 +1,36 @@
+//Mutables e Inmutables
+
+
+//MUTABLES
+var numeroUno=1;
+let numeroDos =2;
+//se puede reasignar otro valor
+numeroUno = 5;
+numeroDos = 8;
+
+numeroUno= false;
+numeroDos = true;
+
+//INMUTABLES
+
+const configuracionArchivos = "PDF";
+//configuracionArchivos ="XML";
+
+//TIPOS DE VARIABLES
+
+const numero =1; //Number
+const sueldo =1.2; //Number
+const texto ="Kevin"; //String
+const booleano = false; //Boolean
+const hijos = null; //Object
+const zapatos = undefined; //undefined
+
+
+
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleano);
+console.log(typeof hijos);
+
+
