be8b965048218197a6a21ca929b0cc8b2bd45686
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-06-12 17:38:13 -0500

    truty falsy

--- 01-Javascript/01-variables.js
+++ 01-Javascript/01-variables.js
@@ -24,6 +24,7 @@ const texto ="Kevin"; //String
 const booleano = false; //Boolean
 const hijos = null; //Object
 const zapatos = undefined; //undefined
+const apellido = 'tipantiza' //String
 
 
 
@@ -32,5 +33,45 @@ console.log(typeof sueldo);
 console.log(typeof texto);
 console.log(typeof booleano);
 console.log(typeof hijos);
+console.log(typeof zapatos);
+console.log(typeof apellido);
+//console.log(typeof NaN);
+
+//console.log(typeof Number(value:"asd")); Transformar
+
+//COMPARACIONES
+
+if (true ==true) {
+    console.log("Es verdadero");
+}else{
+    console.log("Es falso");
+}
+
+if ("") {
+    console.log("Es verdadero");
+}else{
+    console.log("Es falsy"); //!
+}
+
+if ("kevin") {
+    console.log("Es truty");
+}else{
+    console.log("Es falso");
+}
 
 
+if (-1) {
+    console.log("Es truty"); //truty
+}else{
+    console.log("Es falso");
+}
+if (0) {
+    console.log("Es truty");
+}else{
+    console.log("Es falso"); //falsy
+}
+if (1) {
+    console.log("Es truty");//truty
+}else{
+    console.log("Es falso");
+}
