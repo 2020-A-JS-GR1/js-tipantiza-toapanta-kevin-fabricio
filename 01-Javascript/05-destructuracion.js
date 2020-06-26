2e7db48f6b1176305e4a0f75917c46a7048d40f9
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-06-26 16:29:09 -0500

    arreglo y objetos

---
+++ 01-Javascript/05-destructuracion.js
@@ -0,0 +1,13 @@
+const adrian = {
+    nombre:"Adrian"
+};
+
+const carolina = {
+    apellido:"Eguez"
+};
+
+//juntar varios objetos
+
+const arregloUno = [1,2,3,4,5];
+
+const arregloUno = [6,7,8,9,10];
