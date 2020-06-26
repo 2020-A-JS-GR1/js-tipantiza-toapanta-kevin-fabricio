2502b49fdd0d9ad29bd9b05ae730ef54c10c4d07
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-06-26 17:19:16 -0500

    Destructuracion e iniciar callbacks

--- 01-Javascript/05-destructuracion.js
+++ 01-Javascript/05-destructuracion.js
@@ -1,4 +1,4 @@
-const adrian = {
+const adrian = { //creando una nueva referencia
     nombre:"Adrian"
 };
 
@@ -6,8 +6,35 @@ const carolina = {
     apellido:"Eguez"
 };
 
-//juntar varios objetos
+
+//juntar varios objetos y arreglos en uno solo
+
+const adrianCarolina = {
+    ...adrian,
+    ...carolina,
+
+}
+
+adrianCarolina.nombre = "Carolina";
+console.log('adrianCarolina',adrianCarolina);
+
+console.log('adrian',adrian);
+
+
 
 const arregloUno = [1,2,3,4,5];
 
-const arregloUno = [6,7,8,9,10];
+const arregloDos = [6,7,8,9,10];
+
+const superArreglo = [
+    ...arregloUno,
+    ...arregloDos,
+];
+
+superArreglo[0]= 100;
+
+console.log('superArreglo',superArreglo);
+console.log('arregloUno', arregloUno);
+
+
+
