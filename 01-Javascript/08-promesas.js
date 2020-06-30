77fa4149f3660c131a21b683a8c9690bdc5af08b
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-06-30 18:02:23 -0500

    probando promesas concatenadas

--- 01-Javascript/08-promesas.js
+++ 01-Javascript/08-promesas.js
@@ -1,19 +1,59 @@
 const fs = require('fs');
 
-const miPrimerPromesa = new Promise( //Definicion de la promesa
-    (resolve, reject) =>{
-        resolve('Felicidad');
-}
-)
+function promesaEsPar(numero) {
+    const miPrimerPromesa = new Promise( //Definicion de la promesa
+        (resolve, reject) =>{
+            if(numero %2 ==0){
+                resolve(numero);
+            }else{
 
-miPrimerPromesa
-    .then( //try
-    (contenido)=>{
-        console.log('contenido then', contenido);
-    }
+                reject('No es par =(')
+            }
+            //  resolve('Felicidad');
+
+        }
     )
+
+    return miPrimerPromesa
+}
+
+function promesaElevarAlCuadrado(numero) {
+    const miPrimerPromesa = new Promise( //Definicion de la promesa
+        (resolve, reject) =>{
+          //solo va hacer una tarea y no va a fallar jamas--usamos solo resolve
+
+            const numeroElevadoAlCuadrado = Math.pow(numero,2);
+            resolve(numeroElevadoAlCuadrado);
+
+        }
+    );
+
+    return miPrimerPromesa
+}
+
+
+
+
+
+
+
+
+promesaEsPar(2)
+    .then( //try
+    (numeroPar)=>{
+        console.log('contenido then', numeroPar);
+
+
+        return promesaElevarAlCuadrado(numeroPar)//promesa
+    }
+    ).then(
+    (numeroParAlCuadrado) =>{
+        console.log('Numero par al cuadrado: ',numeroParAlCuadrado)
+    }
+)
     .catch( //catch
         (error) =>{
             console.error('contenido catch', error)
         }
     )
+
