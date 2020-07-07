9749a13d66ac672fb8cf016656a201b32775f69c
Kevin Tipantiza <kevin.tipantiza@epn.edu.ec>
2020-07-07 18:08:59 -0500

    explicacion deber

---
+++ 01-Javascript/11-inquirer.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+
+async function main() {
+
+    try {
+        const respuesta = await inquirer
+            .prompt([
+                {
+                    type:'input',
+                    name:'apellido',
+                    message:'ingresa tu nombre',
+                },
+                {
+                    type:'input',
+                    name:'edad',
+                    message:'ingresa tu edad',
+                }
+            ]);
+
+        console.log('Respuesta',respuesta)
+
+    }catch (e) {
+        console.error(e);
+    }
+
+}
+
+main();
